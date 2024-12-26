1f7b55b04eeacd6d018485c652c6032027785f86
Luis Diaz Venero <luisdiazvenero@gmail.com>
2024-12-26 12:24:48 -0500

    ajuste de tiempo de respuesta de servidor en next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Connection',
+            value: 'keep-alive',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
